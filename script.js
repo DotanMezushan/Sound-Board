8532c1a5ecc8abd43e1833abfdd479e657ea1f45
Dotan Mezushan <dotanbm3052@gmail.com>
2021-11-29 10:47:17 +0200

    cat img & sound

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const sounds =['applause','boo','gasp','tada','victory','wrong']
+const sounds =['applause','boo','gasp','tada','victory','wrong','cat']
 sounds.forEach(sound =>{
-   const btn = document.createElement('button')
+if(sound!='cat'){
+    const btn = document.createElement('button')
    btn.classList.add('btn')
 
    btn.innerText = sound
@@ -8,8 +9,23 @@ sounds.forEach(sound =>{
        stopSongs()
        document.getElementById(sound).play()
    })
-
    document.getElementById('buttons').appendChild(btn)
+}
+if(sound ==='cat'){
+    const img =  document.createElement('img')
+    img.src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/1200px-Cat_November_2010-1a.jpg'
+    img.alt='cat'
+    img.classList.add('cat')
+    img.addEventListener('click',()=>{
+        stopSongs()
+        document.getElementById(sound).play()
+    })
+    document.getElementById('buttons').appendChild(img)
+
+}
+   
+
+  
 })
 function stopSongs(){
     sounds.forEach(sound =>{
